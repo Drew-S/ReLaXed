63bc0952a1a5a7a9b3b35ab592be8ccde735f3fb
Drew Sommer <drwsommer@gmail.com>
2018-05-27 19:24:47 -0600

    Fixed merge issues

--- src/generators.js
+++ src/generators.js
@@ -1,60 +1,118 @@
-<<<<<<< HEAD
-exports.bibliography = async function (page) {
-    
+/**
+ * Generate a bibliography from <span class='citation'>
+ * @param {puppeteer.page} page
+ */
+exports.bibliography = async function(page) {
+
+    // Get all the keys from citations
     var values = await page.$$eval('.citation', nodes => {
-        return nodes.map(node => {
-            return node.getAttribute('data-key')
-        })
+      return nodes.map(node => {
+        return node.getAttribute('data-key')
+      })
     // Error occurs because there are no citations
     }).catch(e => { return false })
-
-    if (!values)  return false
-
+  
+    // No citations
+    if (!values) return false
+  
     const Cite = require('citation-js')
-
     const data = new Cite()
-
+  
+    // Add all keys to citation-js
     values.forEach(val => data.add(val))
-
+  
+    // Format the citation spans
     var result = await page.$$eval('.citation', (nodes, data) => {
-        for (var element of nodes) {
-            let key = element.getAttribute('data-key')
-            let page = element.getAttribute('data-page')
-            for (var datum of data) {
-                if (datum.id == key) {
-                if (page != '') {
-                    element.innerHTML = `(${datum.author[0].family}, ${datum.issued['date-parts'][0][0]}, p. ${page})`
-                } else {
-                    element.innerHTML = `(${datum.author[0].family}, ${datum.issued['date-parts'][0][0]})`
-                }
-                break
-                }
+      for (var element of nodes) {
+        let key = element.getAttribute('data-key')
+        let page = element.getAttribute('data-page')
+        for (var datum of data) {
+          if (datum.id == key) {
+            if (page != '') {
+              element.innerHTML = `(${datum.author[0].family}, ${datum.issued['date-parts'][0][0]}, p. ${page})`
+            } else {
+              element.innerHTML = `(${datum.author[0].family}, ${datum.issued['date-parts'][0][0]})`
             }
+            break
+          }
         }
+      }
     }, data.data)
-
+  
+    // Get the bibliography style
+    // TODO: remove with plugin system: define with [//- use-plugin: bibliography <style>] or [config.json]
     var style = await page.$eval('#bibliography', element => {
         return element.getAttribute('data-style')
     // Error occurs because there is no bibliography
     }).catch(e => { return false })
-
+  
+    // No style because no bibliography
     if (!style) return false
-
+  
+    // Format html output for bibliography
     const output = data.get({
-        format: 'string',
-        type: 'html',
-        style: style,
-        lang: 'en-US'
+      format: 'string',
+      type: 'html',
+      style: style,
+      lang: 'en-US'
     })
-
-    var final = await page.$eval('#bibliography', (element, data) => {
-        element.innerHTML = data
+  
+    // Set Bibliography
+    await page.$eval('#bibliography', (element, data) => {
+      element.innerHTML = data
     }, output)
-
+  
     return true
 }
 
 exports.ToC = async function(page, width, height) {
+
+    var convertSize = function(size) {
+        var num = Number(size.match(/\d+\.*\d*/g)[0])
+        var type = /\d+\.*\d*(\w+)/g.exec(size)[1]
+        const ppi = 96
+        const ppc = 2.54 * ppi
+        switch (type) {
+            case 'px':
+                return num
+                break
+            case 'mm':
+                return num * ppc * 10
+                break
+            case 'cm':
+                return num * ppc
+                break
+            case 'in':
+                return num * ppi
+                break
+            default:
+                return num
+                break
+        }
+    }
+    
+    var makeList = function(items) {
+        var depth = 1
+        var list = `<ul class='table-of-contents-list'>`
+        for (var item of items) {
+            if (item.depth > depth) {
+                list += '<ul>'
+                depth++
+            } else if(item.depth < depth) {
+                list += '</ul>'
+                depth--
+            }
+            list += `
+                <li class='ToC-link'
+                    data-linked='${item.id}'
+                    id='${item.id.replace('heading', 'link')}'>
+                    <span class='ToC-link-heading'>${item.text}</span>
+                    <span class='ToC-link-page-number'>{{PAGEHOLDER}}</span>
+                </li>`
+        }
+        return list + '</ul>'
+    }
+
     if (typeof width === 'string') width = convertSize(width)
     else if (!width) width = 8.5 * 96
     if (typeof height === 'string') height = convertSize(height)
@@ -173,121 +231,4 @@ exports.ToC = async function(page, width, height) {
     }, pageNumbers)
 
     return true
-}
-
-function convertSize(size) {
-    var num = Number(size.match(/\d+\.*\d*/g)[0])
-    var type = /\d+\.*\d*(\w+)/g.exec(size)[1]
-    const ppi = 96
-    const ppc = 2.54 * ppi
-    switch (type) {
-        case 'px':
-            return num
-            break
-        case 'mm':
-            return num * ppc * 10
-            break
-        case 'cm':
-            return num * ppc
-            break
-        case 'in':
-            return num * ppi
-            break
-        default:
-            return num
-            break
-    }
-}
-
-function makeList(items) {
-    var depth = 1
-    var list = `<ul class='table-of-contents-list'>`
-    for (var item of items) {
-        if (item.depth > depth) {
-            list += '<ul>'
-            depth++
-        } else if(item.depth < depth) {
-            list += '</ul>'
-            depth--
-        }
-        list += `
-            <li class='ToC-link'
-                data-linked='${item.id}'
-                id='${item.id.replace('heading', 'link')}'>
-                <span class='ToC-link-heading'>${item.text}</span>
-                <span class='ToC-link-page-number'>{{PAGEHOLDER}}</span>
-            </li>`
-    }
-    return list + '</ul>'
-=======
-/**
- * Generate a bibliography from <span class='citation'>
- * @param {puppeteer.page} page
- */
-exports.bibliography = async function(page) {
-
-  // Get all the keys from citations
-  var values = await page.$$eval('.citation', nodes => {
-    return nodes.map(node => {
-      return node.getAttribute('data-key')
-    })
-  }).catch(e => {
-    // Error occurs because there are no citations
-    return false
-  })
-
-  // No citations
-  if (!values) return false
-
-  const Cite = require('citation-js')
-  const data = new Cite()
-
-  // Add all keys to citation-js
-  values.forEach(val => data.add(val))
-
-  // Format the citation spans
-  var result = await page.$$eval('.citation', (nodes, data) => {
-    for (var element of nodes) {
-      let key = element.getAttribute('data-key')
-      let page = element.getAttribute('data-page')
-      for (var datum of data) {
-        if (datum.id == key) {
-          if (page != '') {
-            element.innerHTML = `(${datum.author[0].family}, ${datum.issued['date-parts'][0][0]}, p. ${page})`
-          } else {
-            element.innerHTML = `(${datum.author[0].family}, ${datum.issued['date-parts'][0][0]})`
-          }
-          break
-        }
-      }
-    }
-  }, data.data)
-
-  // Get the bibliography style
-  // TODO: remove with plugin system: define with [//- use-plugin: bibliography <style>] or [config.json]
-  var style = await page.$eval('#bibliography', element => {
-    return element.getAttribute('data-style')
-  }).catch(e => {
-    // Error occurs because there is no bibliography
-    return false
-  })
-
-  // No style because no bibliography
-  if (!style) return false
-
-  // Format html output for bibliography
-  const output = data.get({
-    format: 'string',
-    type: 'html',
-    style: style,
-    lang: 'en-US'
-  })
-
-  // Set Bibliography
-  await page.$eval('#bibliography', (element, data) => {
-    element.innerHTML = data
-  }, output)
-
-  return true
->>>>>>> debb86ff50e779467477e991ed8374af7343d86f
 }
