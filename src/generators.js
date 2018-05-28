a6f188165d2462852202ec6124b39f096f0c3068
Drew Sommer <drwsommer@gmail.com>
2018-05-27 19:45:23 -0600

    Fix size issue (ToC), fully document generators.js

--- src/generators.js
+++ src/generators.js
@@ -1,6 +1,6 @@
 /**
  * Generate a bibliography from <span class='citation'>
- * @param {puppeteer.page} page
+ * @param {puppeteer.page} page - Google Chrome browser tab
  */
 exports.bibliography = async function(page) {
 
@@ -22,7 +22,7 @@ exports.bibliography = async function(page) {
     values.forEach(val => data.add(val))
   
     // Format the citation spans
-    var result = await page.$$eval('.citation', (nodes, data) => {
+    await page.$$eval('.citation', (nodes, data) => {
       for (var element of nodes) {
         let key = element.getAttribute('data-key')
         let page = element.getAttribute('data-page')
@@ -65,13 +65,23 @@ exports.bibliography = async function(page) {
     return true
 }
 
+/**
+ * Generate a Table of Contents from the headings of the document
+ * @param {puppeteer.page} page - Google Chrome browser tab
+ * @param {string} width - The width of the page (not including margins) [px, in, cm, mm]
+ * @param {string} height - The height of the page (not including margins) [px, in, cm, mm]
+ */
 exports.ToC = async function(page, width, height) {
 
+    /**
+     * Convert the string size to pixel size number (string -> number)
+     * @param {string} size - The size of the element to convert to px [px, in, cm, mm]
+     */
     var convertSize = function(size) {
-        var num = Number(size.match(/\d+\.*\d*/g)[0])
-        var type = /\d+\.*\d*(\w+)/g.exec(size)[1]
-        const ppi = 96
-        const ppc = 2.54 * ppi
+        var num = Number(size.match(/\d+\.*\d*/g)[0]) // Get number
+        var type = /\d+\.*\d*(\w+)/g.exec(size)[1]    // Get type ['px', 'in', 'cm', 'mm']
+        const ppi = 96         // Pixels per inch
+        const ppc = 2.54 * ppi // Centimetres per inch
         switch (type) {
             case 'px':
                 return num
@@ -91,7 +101,18 @@ exports.ToC = async function(page, width, height) {
         }
     }
     
+    /**
+     * Generate a table of contents list
+     * @param {items[]} items - List of items with their text, depth, and id's
+     */
     var makeList = function(items) {
+        /*
+        @struct items = {
+            text: string,
+            id: string,
+            depth: integer
+        }
+        */
         var depth = 1
         var list = `<ul class='table-of-contents-list'>`
         for (var item of items) {
@@ -113,25 +134,29 @@ exports.ToC = async function(page, width, height) {
         return list + '</ul>'
     }
 
+    // Convert string version of width/height into pixel numbers
     if (typeof width === 'string') width = convertSize(width)
     else if (!width) width = 8.5 * 96
     if (typeof height === 'string') height = convertSize(height)
     else if (!height) height = 11 * 96
     
+    // Get the max depth
     var depth = await page.$eval('#table-of-contents', ToC => {
         return ToC.getAttribute('data-depth')
     // Error occurs because there is no table of contents
     }).catch(e => { return false })
 
+    // Does not exist, therefore there is no table of contents
     if (!depth) return false
 
     depth = Number(depth)
 
+    // Generate search string for page
     var head = ''
-
     for (var i=1; i<depth; i++) { head += `h${i}, ` }
     head += `h${i}, .new-page`
 
+    // Get the margins of the paper (string of the style)
     var margins = await page.$$eval('style', elements => {
         for (var element of elements) {
             if(/\@page\s*\{[\w\W]*\}/g.test(element.innerHTML)) {
@@ -143,11 +168,21 @@ exports.ToC = async function(page, width, height) {
     var d = '\\s*:\\s*\\d+\\.*\\w+\\s*;'
     var m = 'margin'
 
-    margins = new RegExp(`(${m}${d})|(${m}-top${d})|(${m}-left${d})|(${m}-right${d})|(${m}-bottom${d})`, 'g').exec(margins)
+    margins = new RegExp(`(${m}${d})|(${m}-top${d})|(${m}-left${d})|(${m}-bottom${d})|(${m}-right${d})`, 'g').exec(margins)
 
     var sizes = {}
+    /*
+    @struct sizes = {
+        top: integer,
+        left: integer,
+        bottom: integer,
+        right: integer
+    }
+    */
 
+    // Convert the margin strings to numbers
     if (margins[1] != undefined) {
+        // margins for all sides are the same
         size = convertSize(margins[1])
         sizes = {
             top: size,
@@ -156,6 +191,7 @@ exports.ToC = async function(page, width, height) {
             right: size
         }
     } else {
+        // Individual margins for each side
         var convert = function(ind) {
             if(margins[ind] == undefined) {
                 return 0
@@ -165,16 +201,25 @@ exports.ToC = async function(page, width, height) {
         sizes = {
             top: convert(2),
             left: convert(3),
-            right: convert(4),
-            bottom: convert(5)
+            bottom: convert(4),
+            right: convert(5)
         }
     }
 
+    // Set the body width
     await page.$eval('body', (body, width) => {
         body.width = `${width}px`
     }, width - sizes.left - sizes.right)
 
+    // Get all the headings
     var names = await page.$$eval(head, (elements, width, height) => {
+        /*
+        @struct names = {
+            text: string,
+            id: string,
+            depth: integer
+        }
+        */
         var elementsFixed = []
         var i = 0
         for (var element of elements) {
@@ -194,11 +239,19 @@ exports.ToC = async function(page, width, height) {
         return elementsFixed
     }, width, height - sizes.top - sizes.bottom)
 
+    // Generate a place first pass of the list
     await page.$eval('#table-of-contents', (ToC, list) => {
         ToC.innerHTML = list
     }, makeList(names))
 
+    // Before second pass, get the page numbers 
     var pageNumbers = await page.$$eval(head, (elements, width, height) => {
+        /*
+        @struct pageNumbers = {
+            id: string,
+            page: integer
+        }
+        */
         var elementsFixed = []
         var p = 1
         var newPageError = 0
@@ -219,6 +272,7 @@ exports.ToC = async function(page, width, height) {
         return elementsFixed
     }, width, height)
 
+    // Second pass, replace `{{PAGEHOLDER}}` with actual page number
     await page.$$eval('.ToC-link', (elements, pageNumbers) => {
         for (var element of elements) {
             for (var page of pageNumbers) {
